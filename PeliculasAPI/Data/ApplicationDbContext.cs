a1670adcadaac8b9930dab412e8782f96863d848
alejorom <morojela-github@yahoo.com>
2022-08-31 18:39:15 -0500

    Agregar DBContext

---
+++ PeliculasAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.Models;
+
+namespace PeliculasAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
