78f23020dde71a17e1ce5e9e738f412fc70c89c9
alejorom <morojela-github@yahoo.com>
2022-08-31 18:58:54 -0500

    Agregar repositorio categoria

---
+++ PeliculasAPI/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using PeliculasAPI.Models;
+
+namespace PeliculasAPI.Repository.IRepository
+{
+    public interface ICategoriaRepository
+    {
+        ICollection<Categoria> GetCategorias();
+        Categoria GetCategoria(int CategoriaId);
+        bool ExisteCategoria(string nombre);
+        bool ExisteCategoria(int id);
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool BorrarCategoria(Categoria categoria);
+        bool Guardar();
+
+    }
+}
