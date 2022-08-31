622c2899430e63460195510fe540725c2267b05b
alejorom <morojela-github@yahoo.com>
2022-08-31 18:39:47 -0500

    Agregar modelos pelicula/usuario

---
+++ PeliculasAPI/Models/Pelicula.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeliculasAPI.Models
+{
+
+    public class Pelicula
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    
+        public byte[] RutaImagen { get; set; }
+        public string Descripcion { get; set; }
+        public string Duracion { get; set; }
+        public enum TipoClasificacion { Siete, Trece, Dieciseis, Dieciocho }
+        public TipoClasificacion Clasificacion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        public int categoriaId { get; set; }
+        [ForeignKey("categoriaId")]
+        public Categoria Categoria { get; set; }
+    }
+
+}
