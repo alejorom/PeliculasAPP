61ec9783f6b86c1eacada63c515838d84afd0eb5
alejorom <morojela-github@yahoo.com>
2022-09-01 14:08:53 -0500

    Agregar metodo CrearCategoria HttpPost

--- PeliculasAPI/Controllers/CategoriasController.cs
+++ PeliculasAPI/Controllers/CategoriasController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PeliculasAPI.Models;
 using PeliculasAPI.Models.DTOS;
 using PeliculasAPI.Repository.IRepository;
 
@@ -31,6 +32,50 @@ namespace PeliculasAPI.Controllers
             var listaCategoriasDTO = _mapper.Map<List<CategoriaDTO>>(listaCategorias);
 
             return Ok(listaCategoriasDTO);
+        }
+
+        /// <summary>
+        /// Obtener una categoria de película.
+        /// </summary>
+        /// <param name="categoriaId"></param>
+        /// <returns></returns>
+        [HttpGet("{categoriaId:int}", Name = "GetCategoria")]
+        public IActionResult GetCategoria(int categoriaId)
+        {
+            var itemCategoria = _categoriaRepo.GetCategoria(categoriaId);
+
+            if (itemCategoria == null)
+            {
+                return NotFound();
+            }
+
+            var itemCategoriaDto = _mapper.Map<CategoriaDTO>(itemCategoria);
+            return Ok(itemCategoriaDto);
+        }
+
+        [HttpPost]
+        public IActionResult CrearCategoria([FromBody] CategoriaDTO categoriaDTO)
+        {
+            if (categoriaDTO == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_categoriaRepo.ExisteCategoria(categoriaDTO.Nombre))
+            {
+                ModelState.AddModelError("", "La categoría ya existe");
+                return StatusCode(404, ModelState);
+            }
+
+            var categoria = _mapper.Map<Categoria>(categoriaDTO);
+
+            if (!_categoriaRepo.CrearCategoria(categoria))
+            {
+                ModelState.AddModelError("", $"Algo salio mal guardando el registro {categoria.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetCategoria", new { categoriaId = categoria.Id }, categoria);
         }
     }
 }
