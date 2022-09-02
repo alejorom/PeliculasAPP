dbd85a45b04e9ba4e0451b98d8680b5820366bcd
alejorom <morojela-github@yahoo.com>
2022-09-02 11:29:51 -0500

    Agregar interfaz repositorio de pelicula

---
+++ PeliculasAPI/Repository/IRepository/IPeliculaRepository.cs
@@ -0,0 +1,18 @@
+﻿using PeliculasAPI.Models;
+
+namespace PeliculasAPI.Repository.IRepository
+{
+    public interface IPeliculaRepository
+    {
+        ICollection<Pelicula> GetPeliculas();
+        ICollection<Pelicula> GetPeliculasEnCategoria(int CategoriaId);
+        Pelicula GetPelicula(int PeliculaId);
+        bool ExistePelicula(string nombre);
+        IEnumerable<Pelicula> BuscarPelicula(string nombre);
+        bool ExistePelicula(int id);
+        bool CrearPelicula(Pelicula pelicula);
+        bool ActualizarPelicula(Pelicula pelicula);
+        bool BorrarPelicula(Pelicula pelicula);
+        bool Guardar();
+    }
+}
