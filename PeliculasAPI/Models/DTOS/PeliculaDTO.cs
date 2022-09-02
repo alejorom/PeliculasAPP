2847496d32c1cc25fa37ee38607e752ce57ba74c
alejorom <morojela-github@yahoo.com>
2022-09-02 10:57:18 -0500

    Agregar DTOs de Pelicula

---
+++ PeliculasAPI/Models/DTOS/PeliculaDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static PeliculasAPI.Models.Pelicula;
+
+namespace PeliculasAPI.Models.DTOS
+{
+    public class PeliculaDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        public byte[] RutaImagen { get; set; }
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "La duración es obligatoria")]
+        public string Duracion { get; set; }
+        public TipoClasificacion Clasificacion { get; set; }
+
+        public int categoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
